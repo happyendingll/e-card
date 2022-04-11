1f77cf70422a373c24281ddb765dc31992dfc143
MacServer <942385056@qq.com>
2022-04-11 09:19:49 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Avue from '@smallwei/avue';
+import '@smallwei/avue/lib/index.css';
+import axios from 'axios'
+import 'normalize.css'
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(store).use(router).use(Avue, {axios}).mount('#app')
